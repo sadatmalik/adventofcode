742a34ba1f1ac60ed4f89bea6dfe23bdabb8855f
sadat.malik <sadat.malik@me.com>
2021-12-07 09:38:44 +0000

    Day seven Part 2 - my solution

--- src/com/sadatmalik/aoc/dayseven/MinimumFuelCalculator.java
+++ src/com/sadatmalik/aoc/dayseven/MinimumFuelCalculator.java
@@ -7,10 +7,10 @@ import java.util.Arrays;
 public class MinimumFuelCalculator {
 
     public static void main(String[] args) {
-        // 1. with test data
+        // 1 & 2. with test data
         getMinimumFuel("data/dayseven/testdata.txt");
 
-        // 1. with puzzle data
+        // 1 & 2. with puzzle data
         getMinimumFuel("data/dayseven/puzzledata1.txt");
 
     }
@@ -35,30 +35,35 @@ public class MinimumFuelCalculator {
 
         int[] consumptions = new int[max];
 
-//        int fuelAtMax = 0;
-//        for (int i = 0; i < positions.length; i++) {
-//            fuelAtMax += diff(positions[i], max);
-//        }
-//        System.out.println("Fuel at " + max + " = " + fuelAtMax);
-//        consumptions[max-1] = fuelAtMax;
-//
-//        int fuelAtOne = 0;
-//        for (int i = 0; i < positions.length; i++) {
-//            fuelAtOne += diff(positions[i], 1);
-//        }
-//        System.out.println("Fuel at 1 = " + fuelAtOne);
-//        consumptions[0] = fuelAtOne;
-
-        for (int i = 0; i <= max; i++) {
+        for (int i = 0; i < max; i++) {
             int fuel = 0;
             for (int j = 0; j < positions.length; j++) {
-                fuel += diff(positions[j], i);
+                // 1. fuel += diff(positions[j], i);
+                // 2. fuel += diff2(positions[j], i);
+                fuel += diff2(positions[j], i);
             }
             System.out.println("Fuel at " + i + " = " + fuel);
             consumptions[i] = fuel;
         }
 
         return consumptions;
+    }
+
+    private static int diff2(int start, int end) {
+        int increment = 1;
+        int fuel = 0;
+        while (start < end) {
+            start++;
+            fuel += increment;
+            increment++;
+        }
+        while (start > end) {
+            start--;
+            fuel += increment;
+            increment++;
+        }
+
+        return fuel;
     }
 
     private static int diff(int start, int end) {
