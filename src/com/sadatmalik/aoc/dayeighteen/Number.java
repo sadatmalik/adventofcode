a994f36c4ee08f5026382d9558ec5be7fff9df3e
sadat.malik <sadat.malik@me.com>
2021-12-18 22:01:00 +0000

    Day 18 - implementing add, reduce and explode logic

--- src/com/sadatmalik/aoc/dayeighteen/Number.java
+++ src/com/sadatmalik/aoc/dayeighteen/Number.java
@@ -3,7 +3,11 @@ package com.sadatmalik.aoc.dayeighteen;
 public class Number {
 
     private Node root;
-    private int maxDepth;
+    private int depth;
+    private int maxValue;
+    private Node explodingNode;
+
+    StringBuffer sb;
 
     Number(String number) {
         root = new Node();
@@ -12,11 +16,9 @@ public class Number {
     }
 
     void populate(Node node, String number, int depth) {
-
         if (number.length() == 1) { // ignore last "]"
             return;
         }
-
         char c = number.charAt(0);
         String remainder = number.substring(1);
 
@@ -45,6 +47,160 @@ public class Number {
         if (c == ']') { // go up
             depth--;
             populate(node.parent, remainder, depth);
+        }
+    }
+
+    Number add(Number number) {
+        Node newRoot = new Node();
+        newRoot.left = this.root;
+        newRoot.right = number.root;
+
+        resize(newRoot.left);
+        resize(newRoot.right);
+        
+        this.root = newRoot;
+        reduce();
+        
+        return this;
+    }
+
+    // exposed for testing
+    public void reduce() {
+        depth = 0;
+        getDepth(root);
+        if (depth > 4) {
+            explode(explodingNode);
+            explodingNode = null;
+            reduce();
+            return;
+        }
+
+        maxValue = 0;
+        getMaxValue(root);
+        if (maxValue > 9) {
+            //split(root);
+            reduce();
+        }
+    }
+
+    private void explode(Node node) {
+        Node leftSibling = getLeftSibling(node);
+        if (leftSibling != null) {
+            leftSibling.value += node.left.value;
+        }
+
+        Node rightSibling = getRightSibling(node);
+        if (rightSibling != null) {
+            rightSibling.value += node.right.value;
+        }
+
+        node.left = null;
+        node.right = null;
+        node.value = 0;
+    }
+
+    private Node getLeftSibling(Node node) {
+        if (node.parent == null) {
+            return null;
+        }
+        if (!node.parent.left.equals(node)) {
+            return getRightChild(node.parent.left);
+        }
+        return getLeftSibling(node.parent);
+    }
+
+    private Node getRightChild(Node node) {
+        if (node.value != -1) {
+            return node;
+        }
+        return getRightChild(node.right);
+    }
+
+
+    private Node getRightSibling(Node node) {
+        if (node.parent == null) {
+            return null;
+        }
+        if (!node.parent.right.equals(node)) {
+            return getLeftChild(node.parent.right);
+        }
+        return getRightSibling(node.parent);
+    }
+
+    private Node getLeftChild(Node node) {
+        if (node.value != -1) {
+            return node;
+        }
+        return getLeftChild(node.left);
+    }
+
+
+
+
+    private void getDepth(Node node) {
+        if (node ==  null) {
+            return;
+        }
+
+        if (node.depth != -1) {
+            if (node.depth > depth) {
+                this.depth = node.depth;
+                if (node.depth > 4 && explodingNode == null) {
+                    explodingNode = node.parent;
+                }
+            }
+        }
+        getDepth(node.left);
+        getDepth(node.right);
+    }
+
+    private void getMaxValue(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.value != -1) {
+            if (node.value > maxValue) {
+                this.maxValue = node.value;
+            }
+        }
+
+        getMaxValue(node.left);
+        getMaxValue(node.right);
+    }
+
+    private void resize(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        node.depth++;
+
+        resize(node.left);
+        resize(node.right);
+    }
+
+     public String toString() {
+        sb = new StringBuffer();
+        print(root);
+        return sb.toString();
+    }
+
+    private void print(Node node) {
+        if (node.left != null) {
+            sb.append("[");
+            print(node.left);
+        }
+
+        if (node.value == -1) {
+            sb.append(",");
+        } else {
+            sb.append(node.value);
+        }
+
+        if (node.right != null) {
+            print(node.right);
+            sb.append("]");
         }
     }
 }
--- src/com/sadatmalik/aoc/dayeighteen/TestNumbers.java
+++ src/com/sadatmalik/aoc/dayeighteen/TestNumbers.java
@@ -3,6 +3,34 @@ package com.sadatmalik.aoc.dayeighteen;
 public class TestNumbers {
 
     public static void main(String[] args) {
+        //testNumberPopulation();
+        //testAddition();
+        testReduce();
+    }
+
+    private static void testReduce() {
+        reduce("[[[[[9,8],1],2],3],4]");
+        reduce("[7,[6,[5,[4,[3,2]]]]]");
+        reduce("[[6,[5,[4,[3,2]]]],1]");
+        reduce("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]");
+    }
+
+    private static void reduce(String number) {
+        Number n = new Number(number);
+        System.out.println("Input   = " + n);
+        n.reduce();
+        System.out.println("Reduced = " + n);
+        System.out.println();
+    }
+
+    private static void testAddition() {
+        Number a = new Number("[1,2]");
+        Number b = new Number("[[3,4],5]");
+        Number ab = a.add(b);
+        System.out.println(ab);
+    }
+
+    private static void testNumberPopulation() {
         String number = "[1,2]";
         Number sfNumber = new Number(number);
         System.out.println(sfNumber);
