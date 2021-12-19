2c573b3189ff640018ed14d4e96fb042908c6019
sadat.malik <sadat.malik@me.com>
2021-12-19 09:08:22 +0000

    Day 18 - my solution to part one

--- src/com/sadatmalik/aoc/dayeighteen/SnailFish.java
+++ src/com/sadatmalik/aoc/dayeighteen/SnailFish.java
@@ -1,9 +1,19 @@
 package com.sadatmalik.aoc.dayeighteen;
 
+import com.sadatmalik.aoc.FileReader;
+
+import java.util.List;
+
 public class SnailFish {
 
     public static void main(String[] args) {
-
+        // with puzzle homework
+        List<String> data = FileReader.getDataFromFile("data/dayeighteen/puzzledata.txt");
+        Number number = new Number(data.get(0));
+        System.out.println(number);
+        for (int i = 1; i < data.size(); i++) {
+            number.add(new Number(data.get(i)));
+            System.out.println(number);
+        }
     }
-
 }
